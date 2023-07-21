625eeac5a2cc72fdd3e8fd37f664c1efe1fc86aa
SnoWolfT <ichbinhier@163.com>
2023-07-21 15:04:03 +0800

    Update dnsLoadTest.sh

--- dnsLoadTest.sh
+++ dnsLoadTest.sh
@@ -7,5 +7,6 @@ dnsserver=$2
 while true
 do
   result=$(nslookup -timeout=2 -retry=1 ${dnsname} ${dnsserver} | awk '!a[$0]++' | tr '\n' '|' | tr '\t' ' ')  # remove duplicate line, remove \n and \t for JSON format
-  echo "$(date -u +'%F %H:%M:%S.%3N'),${dnsname},${result}" | grep "timed out"
+  host=$(hostname)
+  echo "$(date -u +'%F %H:%M:%S.%3N'),${host},${dnsname},${result}" | grep "timed out"
 done
