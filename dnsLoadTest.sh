79270051095dc269a0e189bb6156873722efab65
SnoWolfT <ichbinhier@163.com>
2023-07-27 16:56:43 +0800

    Update dnsLoadTest.sh

--- dnsLoadTest.sh
+++ dnsLoadTest.sh
@@ -8,5 +8,6 @@ while true
 do
   result=$(nslookup ${dnsname} ${dnsserver} | awk '!a[$0]++' | tr '\n' '|' | tr '\t' ' ')  # remove duplicate line, remove \n and \t for JSON format
   host=$(hostname)
-  echo "$(date -u +'%F %H:%M:%S.%3N'),${host},${dnsname},${result}" | grep "timed out"
+  ipAddr=$(ifconfig eth0 | grep -v inet6 | grep inet | awk '{print $2}')
+  echo "$(date -u +'%F %H:%M:%S.%3N'),${host},${ipAddr},${dnsname},${result}" | grep "timed out"
 done
